53ec0dfc9244bc71274819763af5b736097df9b9
airbear901 <wolffaar@gmail.com>
2015-08-17 20:22:22 -0400

    Added connect.php file

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php
+
+// User
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$port = 8889;
+$dbname = "myDB";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbname,$port);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
